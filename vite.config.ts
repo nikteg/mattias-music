d88d7446b3f506fcdf6baf39501698fcb940110c
Niklas <me@sodapop.se>
2025-04-14 12:13:53 +0200

    refactor: Conditionally set base path in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/mattias-music/', // Set base path for GitHub Pages
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/', // Default base path for development
+  }
+
+  if (command === 'build') {
+    // Apply base path only for build (production/gh-pages)
+    config.base = '/mattias-music/' 
+  }
+
+  return config
 })
